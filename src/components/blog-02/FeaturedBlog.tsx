2b0a2684819d108c09e0cf09930904de43f079fe
Alexander Oviedo <info@fascinantedigital.com>
2025-09-25 23:01:17 -0400

    debug: agregar colores de prueba a sección FeaturedBlog para debuggear layout

--- src/components/blog-02/FeaturedBlog.tsx
+++ src/components/blog-02/FeaturedBlog.tsx
@@ -7,13 +7,15 @@ const FeaturedBlog = () => {
   const featuredBlogs: IBlogPost[] = getMarkDownData('src/data/blogs').slice(0, 4);
 
   return (
-    <section className="pt-7 pb-14 md:pb-16 lg:pb-20 xl:pb-[100px]">
-      <div className="main-container">
-        <div className="space-y-14 md:space-y-[70px]">
-          <div className="text-center">
+    <section className="pt-7 pb-14 md:pb-16 lg:pb-20 xl:pb-[100px] bg-red-100">
+      <div className="main-container bg-blue-200">
+        <div className="space-y-14 md:space-y-[70px] bg-green-300">
+          <div className="text-center bg-yellow-200">
           </div>
           {/* Featured blog swiper */}
-          <FeaturedBlogSwiper featuredBlogs={featuredBlogs} />
+          <div className="bg-purple-200">
+            <FeaturedBlogSwiper featuredBlogs={featuredBlogs} />
+          </div>
         </div>
       </div>
     </section>
