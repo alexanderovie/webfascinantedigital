a1e18245ce3b7213c5438dc4e91f6421f037b5c3
Alexander Oviedo <info@fascinantedigital.com>
2025-09-28 12:19:12 -0400

    fix: use correct Geist font import paths
    - Import GeistSans from 'geist/font/sans' instead of 'geist/font'
    - Import GeistMono from 'geist/font/mono' instead of 'geist/font'
    - Resolve 'Module not found: Can't resolve geist/font' error

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,4 +1,5 @@
-import { GeistSans, GeistMono } from 'geist/font';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
 const geistSans = GeistSans;
 const geistMono = GeistMono;
