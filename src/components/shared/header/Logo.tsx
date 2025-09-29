c7446013cc9481a2e2db9911599f27823e574d2d
Alexander Oviedo <info@fascinantedigital.com>
2025-09-28 20:02:56 -0400

    clean: remove debug background colors from navbar and logo
    - Remove red background from main navbar container
    - Remove blue background from logo container div
    - Remove green background from logo figure element
    - Remove yellow background from logo image
    - Clean up debug colors after successful logo size adjustment
    - Restore original navbar and logo styling

--- src/components/shared/header/Logo.tsx
+++ src/components/shared/header/Logo.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <div className="bg-blue-500"> {/* 🔵 Color de prueba para el contenedor del logo */}
+    <div>
       <Link href="/">
         <span className="sr-only">Home</span>
-        <figure className="w-[198px] h-[44px] bg-green-500"> {/* 🟢 Color de prueba para el figure - dimensiones originales del SVG */}
-          <Image src={mainLogo} alt="Fascinante Digital" className="dark:invert w-full h-full object-contain bg-yellow-500" /> {/* 🟡 Color de prueba para la imagen */}
+        <figure className="w-[198px] h-[44px]">
+          <Image src={mainLogo} alt="Fascinante Digital" className="dark:invert w-full h-full object-contain" />
         </figure>
       </Link>
     </div>
